c2eb9a7c07e6cf2ba9b6fa0d23f9a6f4d1e2271a
amirhon23 <amirhonahmadov23@gmail.com>
2025-04-04 20:09:27 +0300

    upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let pause = document.querySelector("#pause");
+let set = document.querySelector("#set");
+
+setInterval((btn, idx) =>{
+    pause.onclick = () => {
+                 
+        
+    }
+}, 300)
